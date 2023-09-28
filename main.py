0355bfa33e24ed088a373a5353defa9060d33ca0
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2023-09-28 15:21:44 -0400

    env created and gitignore used

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import requests
+from pprint import pprint
+import os
+
 print("Hello, welcome to my weather API app")
 
-apiKey = "c59053e9cfd68c4fcf70f81e077a7583"
+apiKey = os.environ.get('WEATHER_KEY')
 
-dataLonLat = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={apiKey}").json()
+
+url = f"http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={apiKey}"
+
+dataLonLat = requests.get(url).json()
+query = {"q":"azogues", "units":"metric", "appid":apiKey}
+
+
 
 lat = dataLonLat[0]["lat"]
 lon = dataLonLat[0]["lon"]
 
+weatherURL = 'https://api.openweathermap.org/data/2.5/weather'
 
-dataWeather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}').json()
+dataWeather = requests.get(weatherURL, params=query).json()
 
+pprint(dataWeather)
 print(dataWeather["weather"])
