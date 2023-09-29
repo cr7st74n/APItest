10a883775bef82b7847721c02d724ac0a8f901c8
cr7st74n <cris-082011@hotmail.com>
2023-09-28 22:01:50 -0400

    fixing and cleaning code

--- main.py
+++ main.py
@@ -1,44 +1,56 @@
 import requests
 from pprint import pprint
 import os
-import time
 from datetime import datetime
+import logging
 
+# Key
+apiKey = os.environ.get('WEATHER_KEY')
 # User part 
-print("Hello, welcome to my weather API app")
 
-userCountry = input("Enter the country that you would like to know the 5-day forecast!\n")
+def main():
+    print("Hello, welcome to my weather API app")
+    userCity = ""
+    while len(userCity) == 0:
+        userCity = input("Enter the city that you would like to know the 5-day forecast!\n").strip()
+    weather,error = get_weather(userCity,apiKey)
+    if error:
+        print("Sorry, there is an error with your request.")
+    else:
+        print(f"\nThe weather in {userCity} is: \n")
+        printData = ShowWeather(weather)
+        printData
+
 
 # Get our Data from the API 
-apiKey = os.environ.get('WEATHER_KEY')
+def get_weather(city, key):
+    try:
+        queryForecast = {"q":city, "units":"Imperial","appid":key}
 
-query = {"q":userCountry, "units":"Imperial", "appid":apiKey}
-queryForecast = {"q":userCountry, "units":"Imperial","appid":apiKey}
+        # 5 day Forecast
+        forecastURL = "https://api.openweathermap.org/data/2.5/forecast"
 
-# 5 day Forecast
-forecastURL = "https://api.openweathermap.org/data/2.5/forecast"
-weatherURL = "https://api.openweathermap.org/data/2.5/weather"
+        forecastWeather = requests.get(forecastURL, params=queryForecast).json()
+        # forecastWeather.raise_for_status() # raise exr for 400 or 500 err 
 
-dataWeather = requests.get(weatherURL, params=query).json()
-forecastWeather = requests.get(forecastURL, params=queryForecast).json()
+        forecastList = forecastWeather["list"]
 
+        return forecastList, None
 
-# pprint(dataWeather)
-tempeture = dataWeather["main"]["temp"]
-weatherDescription = dataWeather["weather"][0]["description"]
-windSpeed = dataWeather["wind"]["speed"]
+    except Exception as exc:
+        print(exc)
+        print("Error at getting the API")
+        return None, exc
 
-print(f"The Tempeture for: {userCountry} is: {tempeture}F \n")
-print(f"The weather description is: {weatherDescription} \n")
-print(f"The wind speed is: {windSpeed} \n")
-
-forecastList = forecastWeather["list"]
-
-for days in forecastList:
-
-    print(f"At this time: {datetime.fromtimestamp( days['dt'])} \n")
-    print("the Weather for this day is... \n")
-    print(f"tempeture: {days['main']['temp']} \n")
-    print(f"weather: {days['weather'][0]['description']} \n")
-    print(f"wind: {days['wind']['speed']} \n")
-    # print(f" date: {days['dt_txt']},tempeture: {['main']['temp']}, weather: {days['weather'][0]['description']}, {days['wind']['speed']}")
+# Show to data will print the 5 day Forecast
+def ShowWeather(data):
+    try:
+        for days in data:
+            print(f"At this time: {datetime.fromtimestamp( days['dt'])} \n#the Weather for this day is...") # Unix time here ! Used to show the local time depending on the location.
+            print(f"-tempeture: {days['main']['temp']}F \n-weather Descripton: {days['weather'][0]['description']} \n-wind speed: {days['wind']['speed']} \n")
+    except KeyError:
+        print("The data is not in the correct format")
+        return "Unknown"
+    
+if __name__ == "__main__":
+    main()
